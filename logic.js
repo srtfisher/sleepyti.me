f6818eb88b6adaca44f00e024cfca909390a41f4
Sean Fisher <srtfisher@gmail.com>
2013-05-11 22:35:42 -0400

    Fixing a lot of bugs found on sleepti.me

--- logic.js
+++ logic.js
@@ -75,5 +75,80 @@ module.exports = {
         }
 
         return timeIndex;
+    },
+
+    /**
+     * Calculate logic for specific times
+     */
+    specificTimes: function(ampm, hr, min) {
+        // Sanitize
+        if (min > 59) min = 0;
+        if (hr > 23) hr = 0;
+
+        if (ampm !== 'am' && ampm !== 'pm') ampm = 'am';
+        
+        // Array we're passing to a view
+        timeIndex = [];
+
+        // Count the hours of sleep they'll get
+        hour_count = 9;
+
+        for (var c = 0; c < 4; c++ ) {
+            if (c == 0) {
+                var running_hour = hr - 9;
+
+                if (running_hour < 0)
+                    running_hour = 24 + running_hour;
+
+                running_min = min;
+
+                if (running_hour >= 12)
+                    running_ampm = 'pm';
+                else
+                    running_ampm = 'am';
+            } else {
+                // Increase it by 1.5 hours
+                running_hour += 1;
+                running_min += 30;
+
+                if (running_hour > 23)
+                    running_hour += -24;
+
+                if (running_min > 59)
+                    running_min += -60;
+
+                if (running_hour >= 12)
+                    running_ampm = 'pm';
+                else
+                    running_ampm = 'am';
+            }
+
+            item = {};
+            item.hr = item.human_hr = running_hour;
+
+            if (running_hour > 12)
+                item.human_hr += -12;
+            else if (running_hour == 0)
+                item.human_hr = 12;
+
+            item.human_hr = String(item.human_hr);
+            item.min = running_min;
+
+            if (running_min > 9)
+                item.human_min = String(running_min);
+            else
+                item.human_min = '0' + running_min;
+
+            item.am_pm = running_ampm.toUpperCase();
+
+            item.hours = hour_count;
+
+            timeIndex[c] = item;
+
+            // Lower the hour count
+            hour_count -= 1.5;
+        }
+
+        return timeIndex;
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,17 @@ exports.index = function(req, res){
 };
 
 exports.viewSpecific = function(req, res) {
-    var hour = req.params.hour,
-        minute = req.params.minute,
-        am_pm = req.params.am_pm;
-
+    var ampm = req.params.am_pm.toLowerCase(),
+        hr = parseInt(req.params.hour),
+        min = parseInt(req.params.minute);
+    
+    logic = require('../logic');
+    timeIndex = logic.specificTimes(ampm, hr, min);
+    
     res.render('view_specific', {
-        title: 'sleepytime responsive by srtfisher',
-        hour: hour,
-        minute: minute,
-        am_pm: am_pm
-    })
+        timeIndex: timeIndex,
+        title: 'sleepytime responsive by srtfisher'
+    });
 };
 
 exports.viewNow = function(req, res) {
@@ -46,74 +47,8 @@ exports.viewSpecificInternal = function(req, res) {
         min = parseInt(req.params.minute),
         orig = [hr, min, ampm];
     
-    // Sanitize
-    if (min > 59) min = 0;
-    if (hr > 23) hr = 0;
-
-    if (ampm !== 'am' && ampm !== 'pm') ampm = 'am';
-    console.log(hr + ' ' + min + ' ' + ampm);
-    
-    // Array we're passing to a view
-    timeIndex = [];
-
-    // Count the hours of sleep they'll get
-    hour_count = 9;
-
-    for (var c = 0; c < 4; c++ ) {
-        if (c == 0) {
-            var running_hour = hr - 9;
-
-            if (running_hour < 0)
-                running_hour = 24 + running_hour;
-
-            running_min = min;
-
-            if (running_hour >= 12)
-                running_ampm = 'pm';
-            else
-                running_ampm = 'am';
-        } else {
-            // Increase it by 1.5 hours
-            running_hour += 1;
-            running_min += 30;
-
-            if (running_hour > 23)
-                running_hour += -24;
-
-            if (running_min > 59)
-                running_min += -60;
-
-            if (running_hour >= 12)
-                running_ampm = 'pm';
-            else
-                running_ampm = 'am';
-        }
-
-        item = {};
-        item.hr = item.human_hr = running_hour;
-
-        if (running_hour > 12)
-            item.human_hr += -12;
-        else if (running_hour == 0)
-            item.human_hr = 12;
-
-        item.human_hr = String(item.human_hr);
-        item.min = running_min;
-
-        if (running_min > 9)
-            item.human_min = String(running_min);
-        else
-            item.human_min = '0' + running_min;
-
-        item.am_pm = running_ampm.toUpperCase();
-
-        item.hours = hour_count;
-
-        timeIndex[c] = item;
-
-        // Lower the hour count
-        hour_count -= 1.5;
-    }
+    logic = require('../logic');
+    timeIndex = logic.specificTimes(ampm, hr, min);
 
     res.render('view_specific_ajax', {
         timeIndex: timeIndex
